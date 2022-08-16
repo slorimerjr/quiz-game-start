4c2391b3f5e86748ad6ef91e6f732dc5e5e7f0fb
slorimerjr <slorimerjr@gmail.com>
2022-08-16 16:16:19 -0500

    saving

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,28 @@
-"""Create a method called next_question() - needs to retrieve the item at the current question_number from the question_list.
- Use the input() to show the user the question text and ask for the users answer."""
-
 class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ").lower()
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}")
+
+
+
+
